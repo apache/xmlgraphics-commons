d0cb0ce7aaf1757f32035edbf93b3730ff59cf7b
Jeremias Maerki <jeremias@apache.org>
2006-04-01 18:10:19 +0000

    Ported basic PostScript generation infrastructure from FOP along with supporting utility classes. Jakarta Commons IO is now a dependency as per the plan in the Wiki.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/commons/trunk@390710 13f79535-47bb-0310-9956-ffa450edef68

---
+++ test/java/org/apache/xmlgraphics/util/HexUtil.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* $Id$ */
+
+package org.apache.xmlgraphics.util;
+
+/**
+ * Provides helper functions for converting hexadecimal strings.
+ */
+public class HexUtil {
+
+    private static final char[] DIGITS = 
+        {'0', '1', '2', '3', '4', '5', '6', '7',
+         '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+
+    /**
+     * Converts a byte array to a hexadecimal String
+     * @param data the data to encode
+     * @return String the resulting String
+     */
+    public static final String toHex(byte[] data) {
+        final StringBuffer sb = new StringBuffer(data.length * 2);
+        for (int i = 0; i < data.length; i++) {
+            sb.append(DIGITS[(data[i] >>> 4) & 0x0F]);
+            sb.append(DIGITS[data[i] & 0x0F]);
+        }
+        return sb.toString();
+    }
+    
+}
