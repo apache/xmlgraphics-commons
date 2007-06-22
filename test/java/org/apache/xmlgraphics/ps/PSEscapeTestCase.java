c4988f46a2225322f247e5828156bcff905afe3d
Jeremias Maerki <jeremias@apache.org>
2007-06-22 12:47:05 +0000

    Avoiding characters beyond US-ASCII...
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/commons/trunk@549804 13f79535-47bb-0310-9956-ffa450edef68

--- test/java/org/apache/xmlgraphics/ps/PSEscapeTestCase.java
+++ test/java/org/apache/xmlgraphics/ps/PSEscapeTestCase.java
@@ -45,18 +45,18 @@ public class PSEscapeTestCase extends TestCase {
         PSGenerator.escapeChar(')', sb);
         PSGenerator.escapeChar('\n', sb);
         PSGenerator.escapeChar('\u001E', sb); //<RS>
-        PSGenerator.escapeChar('ä', sb);
+        PSGenerator.escapeChar('\u00E4', sb); //a umlaut
         PSGenerator.escapeChar('\u20AC', sb); //EURO Sign
         assertEquals("0\\t\\(x\\)\\n\\036\\344?", sb.toString());
     }
     
     public void testStringToDSC() throws Exception {
         String escaped;
-        escaped = PSGenerator.convertStringToDSC("0\t(x)\n\u001Eä\u20AC");
+        escaped = PSGenerator.convertStringToDSC("0\t(x)\n\u001E\u00E4\u20AC");
         assertEquals("0\\t\\(x\\)\\n\\036\\344?", escaped);
-        escaped = PSGenerator.convertStringToDSC("0\t(x)\n\u001Eä \u20AC");
+        escaped = PSGenerator.convertStringToDSC("0\t(x)\n\u001E\u00E4 \u20AC");
         assertEquals("(0\\t\\(x\\)\\n\\036\\344 ?)", escaped);
-        escaped = PSGenerator.convertStringToDSC("0\t(x)\n\u001Eä\u20AC", true);
+        escaped = PSGenerator.convertStringToDSC("0\t(x)\n\u001E\u00E4\u20AC", true);
         assertEquals("(0\\t\\(x\\)\\n\\036\\344?)", escaped);
     }
     
