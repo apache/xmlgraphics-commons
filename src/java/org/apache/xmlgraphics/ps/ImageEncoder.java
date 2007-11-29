e14047acdb45edefd1ae0ffc25696f10b8b7b4f6
Jeremias Maerki <jeremias@apache.org>
2007-11-29 11:15:22 +0000

    PostScript: Improved PSImageUtils by introducing the ImageEncoder interface and allowing for streams instead of byte arrays for data transfer which can lower memory consumption. Backwards compatibility is preserved but I've deprecated the methods using byte[]. Copied over PSDictionary from FOP because it is useful in PSImageUtils.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/commons/trunk@599383 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/xmlgraphics/ps/ImageEncoder.java
@@ -0,0 +1,41 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* $Id$ */
+ 
+package org.apache.xmlgraphics.ps;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * The interface is implemented by classes that can generate the raw bitmap field for an image
+ * that might be further encoded/compressed by the image handler class.
+ */
+public interface ImageEncoder {
+
+    /**
+     * Writes the whole raw bitmap field to the given OutputStream. The implementation must not
+     * close the OutputStream when it is finished!
+     * @param out the OutputStream to write to
+     * @throws IOException if an I/O error occurs
+     */
+    void writeTo(OutputStream out) throws IOException;
+    
+    String getImplicitFilter();
+    
+}
