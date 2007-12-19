0b68c883d2fb348d8782d6341ad888060dad7e35
Jeremias Maerki <jeremias@apache.org>
2007-12-19 13:22:20 +0000

    Convenience class if you don't have any special needs for the ImageContext.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/commons/branches/TempNewImagePackage@605534 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/xmlgraphics/image/loader/impl/DefaultImageContext.java
@@ -0,0 +1,45 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* $Id$ */
+
+package org.apache.xmlgraphics.image.loader.impl;
+
+import java.awt.Toolkit;
+
+import org.apache.xmlgraphics.image.loader.ImageContext;
+
+/**
+ * Very simple ImageContext implementation that uses the <code>Toolkit</code>'s screen resolution.
+ */
+public class DefaultImageContext implements ImageContext {
+
+    private float sourceResolution;
+    
+    /**
+     * Main constructor.
+     */
+    public DefaultImageContext() {
+        this.sourceResolution = Toolkit.getDefaultToolkit().getScreenResolution();
+    }
+    
+    /** {@inheritDoc} */
+    public float getSourceResolution() {
+        return this.sourceResolution;
+    }
+
+}
