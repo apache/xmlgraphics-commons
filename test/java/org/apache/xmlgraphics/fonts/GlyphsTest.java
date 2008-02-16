bf2b6a4007664f9f213f8ecdea83701748dca355
Jeremias Maerki <jeremias@apache.org>
2008-02-16 11:58:34 +0000

    Updated Glyphs.java to use the latest Adobe Glyph Lists. The Adobe Glyph Lists are now included as originals and loaded as resources. The license seems to be compatible with the ALv2. The ZapfDingbats glyph names are now also included. Support for the complete mapping process (except characters above 0xFFFF) implemented, i.e. you can use character names such as uniXXXX and uXXXX. Added a mechanism to provide glyph name alternatives which were manually hacked into our old glyph lists.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/commons/trunk@628273 13f79535-47bb-0310-9956-ffa450edef68

--- test/java/org/apache/xmlgraphics/fonts/GlyphsTest.java
+++ test/java/org/apache/xmlgraphics/fonts/GlyphsTest.java
@@ -26,23 +26,68 @@ import junit.framework.TestCase;
  */
 public class GlyphsTest extends TestCase {
 
-    public void testGetUnicodeCodePointsForGlyphName() throws Exception {
+    public void testGetUnicodeSequenceForGlyphName() throws Exception {
         String glyph;
         String unicodes;
         
         glyph = "Omega";
-        unicodes = Glyphs.getUnicodeCodePointsForGlyphName(glyph);
-        assertEquals(2, unicodes.length());
-        assertTrue("Must contain 03A9 - GREEK CAPITAL LETTER OMEGA",
-                unicodes.indexOf("\u03A9") >= 0);
-        assertTrue("Must contain 2126 - OHM SIGN",
-                unicodes.indexOf("\u03A9") >= 0);
+        unicodes = Glyphs.getUnicodeSequenceForGlyphName(glyph);
+        assertEquals(1, unicodes.length());
+        assertEquals("Must contain 2126 - OHM SIGN",
+                unicodes.charAt(0), '\u2126');
+
+        glyph = "Omegagreek";
+        unicodes = Glyphs.getUnicodeSequenceForGlyphName(glyph);
+        assertEquals(1, unicodes.length());
+        assertEquals("Must contain 03A9 - GREEK CAPITAL LETTER OMEGA",
+                unicodes.charAt(0), '\u03A9');
 
         glyph = "A";
-        unicodes = Glyphs.getUnicodeCodePointsForGlyphName(glyph);
+        unicodes = Glyphs.getUnicodeSequenceForGlyphName(glyph);
         assertEquals(1, unicodes.length());
-        assertTrue("Must contain 0041 - LATIN CAPITAL LETTER A",
-                unicodes.indexOf("\u0041") >= 0);
+        assertEquals("Must contain 0041 - LATIN CAPITAL LETTER A",
+                unicodes.charAt(0), '\u0041');
+        
+        glyph = "rehyehaleflamarabic";
+        unicodes = Glyphs.getUnicodeSequenceForGlyphName(glyph);
+        assertEquals(4, unicodes.length());
+        assertEquals("Expected 0631 - ARABIC LETTER REH at position 0",
+                unicodes.charAt(0), '\u0631');
+        assertEquals("Expected FEF3 - ARABIC LETTER YEH INITAL FORM at position 1",
+                unicodes.charAt(1), '\uFEF3');
+        assertEquals("Expected FE8E - ARABIC LETTER ALEF FINAL FORM at position 2",
+                unicodes.charAt(2), '\uFE8E');
+        assertEquals("Expected 0644 - ARABIC LETTER LAM at position 3",
+                unicodes.charAt(3), '\u0644');
+        
+        glyph = "Lcommaaccent_uni20AC0308_u20AC";
+        unicodes = Glyphs.getUnicodeSequenceForGlyphName(glyph);
+        assertEquals(4, unicodes.length());
+        assertEquals("Must contain 013B",
+                unicodes.charAt(0), '\u013B');
+        assertEquals("Must contain 20AC",
+                unicodes.charAt(1), '\u20AC');
+        assertEquals("Must contain 0308",
+                unicodes.charAt(2), '\u0308');
+        assertEquals("Must contain 20AC",
+                unicodes.charAt(3), '\u20AC');
+        
+        glyph = "blah";
+        unicodes = Glyphs.getUnicodeSequenceForGlyphName(glyph);
+        assertNull(unicodes);
     }
     
+    public void testGetCharNameAlternativesFor() throws Exception {
+        String[] alts = Glyphs.getCharNameAlternativesFor("Omega");
+        assertEquals(1, alts.length);
+        assertEquals("Omegagreek", alts[0]);
+
+        alts = Glyphs.getCharNameAlternativesFor("nbspace");
+        assertEquals(2, alts.length);
+        assertEquals("space", alts[0]);
+        assertEquals("nonbreakingspace", alts[1]);
+
+        alts = Glyphs.getCharNameAlternativesFor("A");
+        assertNull(alts);
+    }
 }
