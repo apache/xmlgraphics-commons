c285e3a91acd8628b56cade22a0ecd86e81c2edd
Jeremias Maerki <jeremias@apache.org>
2008-10-23 09:55:37 +0000

    Additional processing hints for bitmap production (color/gray canvas and transparency handling).
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/commons/trunk@707329 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/xmlgraphics/image/loader/ImageProcessingHints.java
+++ src/java/org/apache/xmlgraphics/image/loader/ImageProcessingHints.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,7 +16,7 @@
  */
 
 /* $Id$ */
- 
+
 package org.apache.xmlgraphics.image.loader;
 
 /**
@@ -29,8 +29,35 @@ public interface ImageProcessingHints {
     Object SOURCE_RESOLUTION = "SOURCE_RESOLUTION"; //Value: Number (unit dpi)
     /** Used to send a hint about the target resolution (of the final output format). */
     Object TARGET_RESOLUTION = "TARGET_RESOLUTION"; //Value: Number (unit dpi)
-    
+
     /** Used to tell the image loader to ignore any color profile in the image. */
     Object IGNORE_COLOR_PROFILE = "IGNORE_COLOR_PROFILE"; //Value: Boolean
-    
+
+    /** Used to tell a bitmap producer to generate a certain type of bitmap. */
+    Object BITMAP_TYPE_INTENT = "BITMAP_TYPE_INTENT";
+
+    /**
+     * Used with BITMAP_TYPE_INTENT to indicate that the generated bitmap should be a
+     * grayscale image.
+     */
+    String BITMAP_TYPE_INTENT_GRAY = "gray";
+
+    /**
+     * Used to indicate how existing transparency information (for example, an alpha channel)
+     * shall be treated. */
+    Object TRANSPARENCY_INTENT = "TRANSPARENCY_INTENT";
+
+    /**
+     * Used with TRANSPARENCY_INTENT to indicate that any transparency information shall be
+     * preserved (the default).
+     */
+    String TRANSPARENCY_INTENT_PRESERVE = "preserve";
+
+    /**
+     * Used with TRANSPARENCY_INTENT to indicate that any transparency information shall be
+     * ignored.
+     */
+    String TRANSPARENCY_INTENT_IGNORE = "ignore";
+
+
 }
