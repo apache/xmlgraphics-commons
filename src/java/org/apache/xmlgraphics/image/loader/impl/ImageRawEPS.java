b1ef14895cd329bcf3382596f0885e715555b40e
Jeremias Maerki <jeremias@apache.org>
2008-12-03 08:06:44 +0000

    Made it possible to create cacheable EPS images inside image converters.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/commons/trunk@722804 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/xmlgraphics/image/loader/impl/ImageRawEPS.java
+++ src/java/org/apache/xmlgraphics/image/loader/impl/ImageRawEPS.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,9 +27,18 @@ import org.apache.xmlgraphics.image.loader.ImageInfo;
 
 /**
  * This class is an implementation of the Image interface exposing EPS file. It provides an
- * InputStream to access the EPS content and the decoded high-res bounding box. 
+ * InputStream to access the EPS content and the decoded high-res bounding box.
  */
 public class ImageRawEPS extends ImageRawStream {
+
+    /**
+     * Main constructor.
+     * @param info the image info object
+     * @param streamFactory the InputStreamFactory that is used to create InputStream instances
+     */
+    public ImageRawEPS(ImageInfo info, InputStreamFactory streamFactory) {
+        super(info, ImageFlavor.RAW_EPS, streamFactory);
+    }
 
     /**
      * Main constructor.
@@ -39,7 +48,7 @@ public class ImageRawEPS extends ImageRawStream {
     public ImageRawEPS(ImageInfo info, InputStream in) {
         super(info, ImageFlavor.RAW_EPS, in);
     }
-    
+
     /**
      * Returns the bounding box of the EPS image.
      * @return the bounding box
