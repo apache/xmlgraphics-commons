57e08f8b78013346e8cab14803756cdc447e27c4
Jeremias Maerki <jeremias@apache.org>
2009-12-11 20:36:36 +0000

    More test coverage. Fixed a few smaller bugs. Made null values and empty strings handling more uniform. Removed XMPProperty.clear(). Was a bad idea.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/commons/trunk@889803 13f79535-47bb-0310-9956-ffa450edef68

--- test/java/org/apache/xmlgraphics/xmp/XMPPropertyTest.java
+++ test/java/org/apache/xmlgraphics/xmp/XMPPropertyTest.java
@@ -19,10 +19,17 @@
 
 package org.apache.xmlgraphics.xmp;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
 import junit.framework.TestCase;
 
 import org.apache.xmlgraphics.xmp.schemas.DublinCoreAdapter;
 import org.apache.xmlgraphics.xmp.schemas.DublinCoreSchema;
+import org.apache.xmlgraphics.xmp.schemas.XMPBasicAdapter;
+import org.apache.xmlgraphics.xmp.schemas.XMPBasicSchema;
 
 /**
  * Tests property access methods.
@@ -33,9 +40,15 @@ public class XMPPropertyTest extends TestCase {
         Metadata xmp = new Metadata();
         DublinCoreAdapter dc = DublinCoreSchema.getAdapter(xmp);
         assertNull(dc.getContributors());
+
         dc.addContributor("Contributor1");
         assertEquals(1, dc.getContributors().length);
         assertEquals("Contributor1", dc.getContributors()[0]);
+        dc.removeContributor("Contributor1");
+        assertNull(dc.getContributors());
+
+        dc.addContributor("Contributor1");
+        assertEquals(1, dc.getContributors().length);
         dc.addContributor("Contributor2");
         assertEquals(2, dc.getContributors().length);
         assertFalse(dc.removeContributor("DoesNotExist"));
@@ -45,23 +58,81 @@ public class XMPPropertyTest extends TestCase {
         assertFalse(dc.removeContributor("Contributor2"));
         assertNull(dc.getContributors());
     }
-    
+
     public void testPropertyRemovalLangAlt() throws Exception {
         Metadata xmp = new Metadata();
         DublinCoreAdapter dc = DublinCoreSchema.getAdapter(xmp);
 
         //dc:title is a "Lang Alt"
         dc.setTitle("en", "The title");
-        dc.setTitle("de", "Der Titel");
         String title = dc.removeTitle("en");
+        assertEquals("The title", title);
+        dc.setTitle("en", "The title");
+        dc.setTitle("de", "Der Titel");
+        title = dc.removeTitle("en");
         assertEquals("The title", title);
         title = dc.removeTitle("en");
         assertNull(title);
-        
+
         title = dc.removeTitle("de");
         assertEquals("Der Titel", title);
         title = dc.removeTitle("de");
         assertNull(title);
+    }
+
+    public void testPropertyValues() throws Exception {
+        Metadata xmp = new Metadata();
+        DublinCoreAdapter dc = DublinCoreSchema.getAdapter(xmp);
+
+        String format = dc.getFormat();
+        assertNull(format);
+
+        dc.setFormat("application/pdf");
+        format = dc.getFormat();
+        assertEquals("application/pdf", format);
+
+        dc.setFormat("image/jpeg");
+        format = dc.getFormat();
+        assertEquals("image/jpeg", format);
+
+        dc.setFormat(null);
+        format = dc.getFormat();
+        assertNull(format);
+
+        dc.setFormat(""); //Empty string same as null value
+        format = dc.getFormat();
+        assertNull(format);
+
+        dc.setTitle("title");
+        String title = dc.getTitle();
+        assertEquals("title", title);
+
+        dc.setTitle("Titel");
+        title = dc.getTitle();
+        assertEquals("Titel", title);
+
+        dc.setTitle(null);
+        title = dc.getTitle();
+        assertNull(title);
+
+        dc.setTitle("");
+        title = dc.getTitle();
+        assertNull(title);
+    }
+
+    public void testDates() throws Exception {
+        Metadata xmp = new Metadata();
+        XMPBasicAdapter basic = XMPBasicSchema.getAdapter(xmp);
+
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"), Locale.ENGLISH);
+        cal.set(2008, Calendar.FEBRUARY, 07, 15, 11, 07);
+        cal.set(Calendar.MILLISECOND, 0);
+        Date dt = cal.getTime();
+
+        assertNull(basic.getCreateDate());
+        basic.setCreateDate(dt);
+        Date dt2 = basic.getCreateDate();
+        assertEquals(dt2, dt);
     }
 
 }
