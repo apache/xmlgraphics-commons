895bffe311531b56e9827773101a5a7b32eba197
Jeremias Maerki <jeremias@apache.org>
2010-06-29 13:59:58 +0000

    Added a parser for ICC named color profiles.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/commons/branches/Temp_Color@958970 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/xmlgraphics/java2d/color/profile/NamedColorProfile.java
@@ -0,0 +1,106 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* $Id$ */
+
+package org.apache.xmlgraphics.java2d.color.profile;
+
+import java.awt.color.ICC_Profile;
+
+import org.apache.xmlgraphics.java2d.color.NamedColorSpace;
+
+/**
+ * Simplified in-memory representation of an ICC named color profile.
+ */
+public class NamedColorProfile {
+
+    private String profileName;
+    private String copyright;
+    private NamedColorSpace[] namedColors;
+    private int renderingIntent = ICC_Profile.icPerceptual;
+
+    /**
+     * Creates a new named color profile.
+     * @param profileName the profile name
+     * @param copyright the copyright
+     * @param namedColors the array of named colors
+     */
+    public NamedColorProfile(String profileName, String copyright, NamedColorSpace[] namedColors) {
+        this.profileName = profileName;
+        this.copyright = copyright;
+        this.namedColors = namedColors;
+    }
+
+    /**
+     * Returns the color profile's rendering intent.
+     * @return the rendering intent (See {@link ICC_Profile}.ic*)
+     */
+    public int getRenderingIntent() {
+        return this.renderingIntent;
+    }
+
+    /**
+     * Returns the array of named colors.
+     * @return the array of named colors
+     */
+    public NamedColorSpace[] getNamedColors() {
+        NamedColorSpace[] copy = new NamedColorSpace[this.namedColors.length];
+        System.arraycopy(this.namedColors, 0, copy, 0, this.namedColors.length);
+        return copy;
+    }
+
+    /**
+     * Returns a named color.
+     * @param name the color name
+     * @return the named color (or null if it is not available)
+     */
+    public NamedColorSpace getNamedColor(String name) {
+        if (this.namedColors != null) {
+            for (int i = 0, c = this.namedColors.length; i < c; i++) {
+                if (this.namedColors[i].getColorName().equals(name)) {
+                    return this.namedColors[i];
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Returns the profile name.
+     * @return the profile name
+     */
+    public String getProfileName() {
+        return this.profileName;
+    }
+
+    /**
+     * Returns the profile copyright.
+     * @return the profile copyright
+     */
+    public String getCopyright() {
+        return this.copyright;
+    }
+
+    /** {@inheritDoc} */
+    public String toString() {
+        StringBuffer sb = new StringBuffer("Named color profile: ");
+        sb.append(getProfileName());
+        sb.append(", ").append(namedColors.length).append(" colors");
+        return sb.toString();
+    }
+
+}
