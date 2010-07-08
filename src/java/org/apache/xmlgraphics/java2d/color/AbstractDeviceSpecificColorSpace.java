345e284f8b27d75608ed10016113b8d450d9b8c3
Jeremias Maerki <jeremias@apache.org>
2010-07-08 08:43:25 +0000

    Added missing serial version UIDs.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/commons/branches/Temp_Color@961632 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/xmlgraphics/java2d/color/AbstractDeviceSpecificColorSpace.java
+++ src/java/org/apache/xmlgraphics/java2d/color/AbstractDeviceSpecificColorSpace.java
@@ -26,6 +26,8 @@ import java.awt.color.ColorSpace;
  */
 public abstract class AbstractDeviceSpecificColorSpace extends ColorSpace {
 
+    private static final long serialVersionUID = -4888985582872101875L;
+
     protected AbstractDeviceSpecificColorSpace(int type, int numcomponents) {
         super(type, numcomponents);
     }
