52e43507ce1bb6dedee2c5e9ae27914db85f6b82
Jeremias Maerki <jeremias@apache.org>
2010-09-02 07:55:21 +0000

    ImageWriterRegistry: Change default order to use negative numbers so the default register() method already overrides the default implementations when using the zero priority. Added test case for the ImageWriterRegistry.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/commons/trunk@991838 13f79535-47bb-0310-9956-ffa450edef68

---
+++ test/java/org/apache/xmlgraphics/image/writer/ImageWriterRegistryTest.java
@@ -0,0 +1,76 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* $Id$ */
+
+package org.apache.xmlgraphics.image.writer;
+
+import java.awt.image.RenderedImage;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import junit.framework.TestCase;
+
+import org.apache.xmlgraphics.image.writer.imageio.ImageIOPNGImageWriter;
+
+/**
+ * Tests the {@link ImageWriterRegistry}.
+ */
+public class ImageWriterRegistryTest extends TestCase {
+
+    public void testRegistry() throws Exception {
+        ImageWriterRegistry registry = new ImageWriterRegistry();
+
+        ImageWriter writer;
+        writer = registry.getWriterFor("image/something");
+        assertNull(writer);
+
+        writer = registry.getWriterFor("image/png");
+        assertTrue(writer instanceof ImageIOPNGImageWriter);
+
+        registry.register(new DummyPNGWriter());
+
+        ImageWriter dummy = registry.getWriterFor("image/png");
+        assertEquals(DummyPNGWriter.class, dummy.getClass());
+
+        registry.register(new OtherPNGWriter(), 50);
+
+        dummy = registry.getWriterFor("image/png");
+        assertEquals(OtherPNGWriter.class, dummy.getClass());
+    }
+
+    private static class DummyPNGWriter extends AbstractImageWriter {
+
+        public String getMIMEType() {
+            return "image/png";
+        }
+
+        public void writeImage(RenderedImage image, OutputStream out) throws IOException {
+            //nop
+        }
+
+        public void writeImage(RenderedImage image, OutputStream out, ImageWriterParams params)
+                throws IOException {
+            //nop
+        }
+
+    }
+
+    private static class OtherPNGWriter extends DummyPNGWriter {
+
+    }
+}
