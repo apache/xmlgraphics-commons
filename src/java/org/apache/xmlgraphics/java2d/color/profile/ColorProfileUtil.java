f8eb9d091542ce3e262500a6236083d9b82e553e
Andreas L. Delmelle <adelmelle@apache.org>
2011-02-01 19:54:23 +0000

    Synchronize access to java.awt.color.ICC_Profile (see also Bugzilla 50698)
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/commons/trunk@1066179 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/xmlgraphics/java2d/color/profile/ColorProfileUtil.java
+++ src/java/org/apache/xmlgraphics/java2d/color/profile/ColorProfileUtil.java
@@ -17,11 +17,13 @@
 
 /* $Id$ */
 
-package org.apache.fop.util;
+package org.apache.xmlgraphics.java2d.color.profile;
 
 import java.awt.color.ColorSpace;
 import java.awt.color.ICC_ColorSpace;
 import java.awt.color.ICC_Profile;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 
 /**
@@ -66,6 +68,66 @@ public final class ColorProfileUtil {
             sRGBProfile = ((ICC_ColorSpace)sRGB).getProfile();
         }
         return profile == sRGBProfile;
+    }
+
+    /**
+     * Proxy method for {@link ICC_Profile#getInstance(byte[])}
+     * that properly synchronizes the call to avoid a potential race condition.
+     * @param data    the specified ICC Profile data
+     * @return  an {@link ICC_Profile} instance corresponding to the data in the
+     *          specified byte array
+     */
+    public static ICC_Profile getICC_Profile(byte[] data) {
+        synchronized (ICC_Profile.class) {
+            return ICC_Profile.getInstance(data);
+        }
+    }
+
+    /**
+     * Proxy method for {@link ICC_Profile#getInstance(int)}
+     * that properly synchronizes the call to avoid a potential race condition.
+     * @param colorSpace    the type of color space to create a profile for. The specified type is
+     *                      one of the color space constants defined in the {@link ColorSpace}
+     *                      class.
+     * @return  an {@link ICC_Profile} instance corresponding to the specified {@code ColorSpace}
+     * @throws IllegalArgumentException if {@code colorSpace} is not one of the predefined types
+     */
+    public static ICC_Profile getICC_Profile(int colorSpace) {
+        synchronized (ICC_Profile.class) {
+            return ICC_Profile.getInstance(colorSpace);
+        }
+    }
+
+    /**
+     * Proxy method for {@link ICC_Profile#getInstance(java.io.InputStream)}
+     * that properly synchronizes the call to avoid a potential race condition.
+     * @param in    the input stream from which to read the profile data
+     * @return  an {@link ICC_Profile} instance corresponding to the data in the
+     *          specified {@link InputStream}
+     * @throws IOException  if an I/O error occurs while reading the stream
+     * @throws IllegalArgumentException if the stream does not contain valid ICC Profile data
+     */
+    public static ICC_Profile getICC_Profile(InputStream in) throws IOException {
+        synchronized (ICC_Profile.class) {
+            return ICC_Profile.getInstance(in);
+        }
+    }
+
+    /**
+     * Proxy method for {@link ICC_Profile#getInstance(java.lang.String)}
+     * that properly synchronizes the call to avoid a potential race condition.
+     * @param fileName    the name of the file that contains the profile data
+     * @return  an {@link ICC_Profile} instance corresponding to the data in the specified file
+     * @throws IOException  if the file cannot be opened, or an I/O error occurs while reading
+     *          the stream
+     * @throws IllegalArgumentException if the stream does not contain valid ICC Profile data
+     * @throws SecurityException if a security manager is installed and it does not permit read
+     *          access to the given file.
+     */
+    public static ICC_Profile getICC_Profile(String fileName) throws IOException {
+        synchronized (ICC_Profile.class) {
+            return ICC_Profile.getInstance(fileName);
+        }
     }
 
 }
