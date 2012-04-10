642c31aec99902e706bef08453aa158bc70eee1c
Glenn Adams <gadams@apache.org>
2012-04-10 18:44:31 +0000

    Bugzilla #47174: Should never use Error to report runtime errors.
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/commons/trunk@1311918 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/xmlgraphics/fonts/Glyphs.java
+++ src/java/org/apache/xmlgraphics/fonts/Glyphs.java
@@ -646,7 +646,7 @@ public class Glyphs {
         List lines = new java.util.ArrayList();
         InputStream in = Glyphs.class.getResourceAsStream(filename);
         if (in == null) {
-            throw new Error("Cannot load " + filename
+            throw new RuntimeException("Cannot load " + filename
                     + ". The Glyphs class cannot properly be initialized!");
         }
         try {
@@ -658,10 +658,10 @@ public class Glyphs {
                 }
             }
         } catch (UnsupportedEncodingException uee) {
-            throw new Error("Incompatible JVM! US-ASCII encoding is not supported."
+            throw new RuntimeException("Incompatible JVM! US-ASCII encoding is not supported."
                     + " The Glyphs class cannot properly be initialized!");
         } catch (IOException ioe) {
-            throw new Error("I/O error while loading " + filename
+            throw new RuntimeException("I/O error while loading " + filename
                     + ". The Glyphs class cannot properly be initialized!");
         } finally {
             IOUtils.closeQuietly(in);
