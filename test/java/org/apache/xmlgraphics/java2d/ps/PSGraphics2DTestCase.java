154ca34df896a5c6ddefd5e8bbed11532b9da640
Robert Meyer <rmeyer@apache.org>
2013-10-18 15:37:35 +0000

    FOP-2304: SVN line clipping not correct when outputting to Postscript
    git-svn-id: https://svn.apache.org/repos/asf/xmlgraphics/commons/trunk@1533516 13f79535-47bb-0310-9956-ffa450edef68

--- test/java/org/apache/xmlgraphics/java2d/ps/PSGraphics2DTestCase.java
+++ test/java/org/apache/xmlgraphics/java2d/ps/PSGraphics2DTestCase.java
@@ -20,21 +20,26 @@
 package org.apache.xmlgraphics.java2d.ps;
 
 import java.awt.Rectangle;
+import java.awt.Shape;
 import java.awt.geom.AffineTransform;
+import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
 import java.io.IOException;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import org.apache.xmlgraphics.java2d.GraphicContext;
+import org.apache.xmlgraphics.ps.PSGenerator;
+import org.apache.xmlgraphics.ps.PSState;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-
-import org.apache.xmlgraphics.java2d.GraphicContext;
-import org.apache.xmlgraphics.ps.PSGenerator;
-import org.apache.xmlgraphics.ps.PSState;
 
 public class PSGraphics2DTestCase {
 
@@ -61,5 +66,14 @@ public class PSGraphics2DTestCase {
         assertEquals(gfx2d.getTransform(), transform);
         gfx2d.draw(new Rectangle(10, 10, 100, 100));
         verify(gen, times(1)).concatMatrix(transform);
+    }
+
+    @Test
+    public void testShouldBeClipped() {
+        Shape line = new Line2D.Float(10, 10, 50, 50);
+        Shape clipArea = new Rectangle2D.Float(20, 20, 100, 100);
+        assertTrue(gfx2d.shouldBeClipped(clipArea, line));
+        Shape rect = new Rectangle2D.Float(30, 30, 40, 40);
+        assertFalse(gfx2d.shouldBeClipped(clipArea, rect));
     }
 }
